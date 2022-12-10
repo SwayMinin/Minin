a0f07979f62a9faa3e39f3667d74760d2cc1a05b
swayminin <swayminin@yandex.ru>
2022-12-10 21:04:10 +0500

    Выбор вывода данных

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from report import Report
 
 
 if __name__ == "__main__":
+    output_data = input('Введите данные для печати: ')
     inp_connect = InputConnect()
-    Report.generate_pdf(inp_connect)
+    if output_data == 'Вакансии':
+        inp_connect.print_vacancies()
+    if output_data == 'Статистика':
+        Report.generate_pdf(inp_connect)

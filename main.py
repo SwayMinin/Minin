9757bf10cc59bcd334525855fa7321d15d5d6d0b
swayminin <swayminin@yandex.ru>
2022-12-31 16:52:08 +0500

    Коммит с конфликтом?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from report import Report
 
 
 if __name__ == "__main__":
-    output_data = input('Ёпта Введите данные для печати: ')
+    output_data = input('dasdadasdasdВведите данные для печати: ')
     inp_connect = InputConnect()
     if output_data == 'Вакансии':
         inp_connect.print_vacancies()
